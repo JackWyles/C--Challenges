3eb2b36b22ee3617a898c7d9da27471f91b5230a
Jack Wyles <56089320+JackWyles@users.noreply.github.com>
2019-10-02 20:06:01 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,74 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _11_GradeBoundaries
+namespace _12_RPG_Dice
 {
     class Program
     {
-        /*
-		<2	U
-		2	1
-		4	2
-		13	3
-		22	4
-		31	5
-		41	6
-		54	7
-		67	8
-		80	9
-
-         */
         static void Main(string[] args)
         {
-            Console.Write("Enter your mark: ");
-            int score = Convert.ToInt32(Console.ReadLine());
-            if(score < 2)
+            Console.Write("Enter the amount of faces on the die: ");
+            int max = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            while (true)
             {
-                int missed = 2 - score;
-                Console.WriteLine("You achieved a grade U. "+missed.ToString()+" off a 1.");
-            } else if (score < 4)
-            {
-                int missed = 4 - score;
-                Console.WriteLine("You achieved a grade 1. " + missed.ToString() + " off a 2.");
-            } else if(score < 13)
-            {
-                int missed = 13 - score;
-                Console.WriteLine("You achieved a grade 2. " + missed + " off a 3.");
+                Console.WriteLine("Press Enter to roll the die, press Q to quit");
+                var key = Console.ReadKey();
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    Console.WriteLine("You rolled: " + random.Next(1, max + 1).ToString());
+                }
+                else if (key.Key == ConsoleKey.Q)
+                {
+                    break;
+                }
             }
-            else if (score < 22)
-            {
-                int missed = 22 - score;
-                Console.WriteLine("You achieved a grade 3. " + missed.ToString() + " off a 4.");
-            }
-            else if (score < 31)
-            {
-                int missed = 31 - score;
-                Console.WriteLine("You achieved a grade 4. " + missed + " off a 5.");
-            }
-            else if (score < 41)
-            {
-                int missed = 41 - score;
-                Console.WriteLine("You achieved a grade 5. " + missed.ToString() + " off a 6.");
-            }
-            else if (score < 54)
-            {
-                int missed = 54 - score;
-                Console.WriteLine("You achieved a grade 6. " + missed + " off a 7.");
-            }
-            else if (score < 67)
-            {
-                int missed = 67 - score;
-                Console.WriteLine("You achieved a grade 7. " + missed.ToString() + " off a 8.");
-            }
-            else if (score < 80)
-            {
-                int missed = 80 - score;
-                Console.WriteLine("You achieved a grade 8. " + missed + " off a 9.");
-            }
-            else
-            {
-                Console.WriteLine("You achieved a 9. Good job!");
-            }Console.ReadLine();
         }
     }
 }
